845449c0969c09297853f305debf3ad6a37cd124
Mohd Saim <133566485+saimgit629@users.noreply.github.com>
2023-05-26 10:19:13 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -20,9 +20,6 @@ else
 
     mail("saimmohd629@gmail.com", "webstreet", $message , "From: $name <$email>");
 
-    echo "<script type='text/javascript'> alert ('your message sent successfully')
-    
-    window.history.log(-1);
-    </script>";
+   
 }
 ?>
